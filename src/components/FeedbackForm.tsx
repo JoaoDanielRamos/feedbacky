24324572a17e7eeac350431284b2ce07107db39f
João Ramos <jdmor95@gmail.com>
2022-04-18 00:41:09 -0700

    added rating select component, but still missing the form submit.

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from 'react';
 import Card from './shared/Card';
 import Button from './shared/Button';
+import RatingSelect from './RatingSelect';
 
 export default function FeedbackForm() {
   const [text, setText] = useState('');
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState('');
+  const [rating, setRating] = useState(10);
 
   const handleTextChange = (event: any) => {
-    console.log('1', event.target.value);
-
     if (text === '') {
       setMessage('');
-      setBtnDisabled(false);
-    } else if (text !== '' && text.trim().length <= 10) {
+      setBtnDisabled(true);
+    } else if (text !== '' && text.length <= 10) {
       setBtnDisabled(true);
       setMessage('Your review must be at least 10 characters long.');
     } else {
@@ -22,27 +22,29 @@ export default function FeedbackForm() {
     }
 
     setText(event.target.value);
-    console.log(text);
-    console.log(text.length);
   };
 
   return (
     <Card reverse={false}>
       <form>
         <h2>How would you rate your service with us?</h2>
-        {/* @todo - rating select component  */}
-      </form>
-      <div className='input-group'>
-        <input
-          onChange={handleTextChange}
-          type='text'
-          placeholder='Write a review'
+        <RatingSelect
+          select={(rating: number) => {
+            setRating(rating);
+          }}
         />
-        <Button type='submit' version={'secondary'} isDisabled={btnDisabled}>
-          Send
-        </Button>
-      </div>
-      <p>{message}</p>
+        <div className='input-group'>
+          <input
+            onChange={handleTextChange}
+            type='text'
+            placeholder='Write a review'
+          />
+          <Button type='submit' version={'secondary'} isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+        <p>{message}</p>
+      </form>
     </Card>
   );
 }
