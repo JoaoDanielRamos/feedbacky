1c92af2b9aa96e900fc8fd5c904846a642ef0c7c
João Ramos <jdmor95@gmail.com>
2022-04-17 21:09:16 -0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+//Modules
+import { useState } from 'react';
+import FeedbackData from './data/FeedbackData';
+
 // Components
 import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
   return (
     <div className='App'>
       <Header />
       <div className='container'>
-        <h1>My app</h1>
+        <FeedbackList feedback={feedback} />
       </div>
     </div>
   );
