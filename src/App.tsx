3baf8383fd313ac4cc6b5983c4e10e826ec94787
João Ramos <jdmor95@gmail.com>
2022-04-20 02:30:13 -0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // * Modules
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 // * Components
 import Header from './components/Header';
